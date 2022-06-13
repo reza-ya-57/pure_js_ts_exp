72b26e999be80afb42cb5dd852ce86e6f4037c38
reza-yari <rezayari.57@gmail.com>
2022-06-13 22:00:46 +0430

    first commit

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+
+
+// var last = "reza"
+// console.log(last)
+
+// var obj = {
+//     name: "reza" ,
+//     lastName: "yari"
+// } as const
+
+// obj.name = "mamad"
+
+// console.log(obj)
---
+++ ts-built/main.js
@@ -0,0 +1,8 @@
+// var last = "reza"
+// console.log(last)
+// var obj = {
+//     name: "reza" ,
+//     lastName: "yari"
+// } as const
+// obj.name = "mamad"
+// console.log(obj)
